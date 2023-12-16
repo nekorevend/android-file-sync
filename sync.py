a01becac8308966b29fbabd5687697add7005762
Victor Chang <victor.chang.1024@gmail.com>
2023-12-16 01:24:48 -0800

    Do integrity checking and re-copying.
    Inefficient and kind of messy, but it works.

--- sync.py
+++ sync.py
@@ -4,39 +4,54 @@ import re
 import sys
 import time
 import paramiko
+import hashlib
 from paramiko import RSAKey
 from scp import SCPClient
-from queue import PriorityQueue
 
 # This is the official delimiter character, ASCII code 30... Too bad the industry settled on commas.
 # Hopefully this will have less chance of conflicting with filenames.
-record_separator = '\u001E'
+record_separator = "\u001E"
 
 
 class Client:
-    def __init__(self, ip, ssh_client, scp_client, destination_path, limit_gb, port=22, username='root'):
+    def __init__(
+        self,
+        ip,
+        ssh_client,
+        scp_client,
+        source_path,
+        destination_path,
+        limit_gb,
+        port=22,
+        username="root",
+    ):
         self.ip = ip
         self.port = port
         self.username = username
+        self.source_path = source_path
         self.destination_path = destination_path
-        self.storage_limit_bytes = limit_gb * (1024 ** 3)
+        self.storage_limit_bytes = limit_gb * (1024**3)
 
-        self.oldest_date_at_dest = time.time()  # initialize to now, but will be overwritten.
-        self.existing_files_pq = PriorityQueue()
+        self.oldest_date_at_dest = (
+            time.time()
+        )  # initialize to now, but will be overwritten.
+        self.existing_files = []
         self.existing_filenames = set()
         self.size_of_existing_files = 0
-        self.source_files_pq = PriorityQueue()
+        self.source_files = []
         self.size_of_new_files = 0
+
+        self.bad_files = []
 
         self.ssh = ssh_client
         self.scp = scp_client
-    
+
     def __del__(self):
         if self.scp:
             self.scp.close()
         if self.ssh:
             self.ssh.close()
-    
+
     def _ssh_command(self, command):
         _, stdout, stderr = self.ssh.exec_command(command)
         output = stdout.read().decode()
@@ -45,86 +60,156 @@ class Client:
             print("Error:", file=sys.stderr)
             print(error, file=sys.stderr)
         return output
-    
+
     def _scp_command(self, source_path, dest_path):
         self.scp.put(source_path, dest_path, preserve_times=True)
-    
-    def _scan_source_files(self, source_path):
-        with os.scandir(source_path) as it:
+
+    def _scan_source_files(self):
+        with os.scandir(self.source_path) as it:
             for entry in it:
                 if entry.is_file():
                     size_bytes = entry.stat().st_size
                     modified_date = int(os.path.getmtime(entry.path))
                     filename = entry.name
-                    if modified_date < self.oldest_date_at_dest or filename in self.existing_filenames:
+                    if (
+                        modified_date < self.oldest_date_at_dest
+                        or filename in self.existing_filenames
+                    ):
                         continue
-                    self.source_files_pq.put((modified_date, filename, size_bytes))
+                    self.source_files.append((modified_date, filename, size_bytes))
                     self.size_of_new_files += size_bytes
-    
+
+        self.source_files.sort()
+
     def _scan_destination_files(self):
         command = f"""
         for file in /sdcard/sync/{self.destination_path}/*; do
-            echo "$(basename "$file"){record_separator}$(stat -c %Y "$file"){record_separator}$(stat -c %s "$file")"
+            echo "$(basename "$file"){record_separator}$(stat -c %Y "$file"){record_separator}$(stat -c %s "$file"){record_separator}$(md5sum -b "$file")"
         done
         """
         output = self._ssh_command(command)
-        lines = output.split('\n')  # splitlines() considers ASCII 30 to be equivalent to newline. (╯°□°)╯︵ ┻━┻
+        lines = output.split(
+            "\n"
+        )  # splitlines() considers ASCII 30 to be equivalent to newline. (╯°□°)╯︵ ┻━┻
         for line in lines:
             items = line.split(record_separator)
-            if len(items) != 3:
+            if len(items) < 4:
                 continue
+            md5 = items[3]
             size_bytes = int(items[2])
             modified_date = int(items[1])
             filename = items[0]
-            # print(f"{modified_date} : {filename} : {size_bytes}")
+            # print(f"{modified_date} : {filename} : {size_bytes} : {md5}")
             self.oldest_date_at_dest = min(self.oldest_date_at_dest, modified_date)
-            self.existing_files_pq.put((modified_date, filename, size_bytes))
+            self.existing_files.append((modified_date, filename, size_bytes, md5))
             self.existing_filenames.add(filename)
             self.size_of_existing_files += size_bytes
 
-    def _delete_existing_file(self, name, size):
-        self._ssh_command(f'rm /sdcard/sync/{self.destination_path}/{name}')
-        self.size_of_existing_files -= size
-        self.existing_filenames.remove(name)
-    
+        self.existing_files.sort()
+
+    def _check_integrity(self, source_dir, new_files=None):
+        for _, filename, _, md5 in self.existing_files:
+            path = os.path.join(source_dir, filename)
+
+            # Skip if the file does not exist.
+            if not os.path.exists(path):
+                continue
+
+            # Open the file in binary mode for md5 calculation
+            with open(path, "rb") as f:
+                source_content = f.read()
+                if hashlib.md5(source_content).hexdigest().lower() != md5.lower():
+                    self.bad_files.append(filename)
+
+    def _create_ready_file(self):
+        self._ssh_command(f"touch /sdcard/sync/{self.destination_path}/ready_to_scan")
+
+    def _delete_existing_file(self, name, size=None):
+        self._ssh_command(f"rm /sdcard/sync/{self.destination_path}/{name}")
+        if size:
+            self.size_of_existing_files -= size
+            self.existing_filenames.remove(name)
+            self.existing_files.pop(0)
+
     def _make_space(self, amount_bytes):
         if amount_bytes == 0:
             return
 
-        print('Freeing up', amount_bytes, 'bytes of space on the phone...')
+        print("Freeing up", amount_bytes, "bytes of space on the phone...")
 
-        while not self.existing_files_pq.empty():
-            _, name, size = self.existing_files_pq.get()
-            print('Deleting from phone:', name)
+        for _, name, size, _ in self.existing_files.copy():
+            print("Deleting from phone:", name)
             self._delete_existing_file(name, size)
             amount_bytes -= size
             if amount_bytes <= 0:
                 return
-    
-    def _copy_over_file(self, file_dir, file_name, size):
-        available_space = self.storage_limit_bytes - self.size_of_existing_files
-        if available_space < size:
-            self._make_space(size - available_space)
-        print('Copying', file_dir + file_name, 'to', f'/sdcard/sync/{self.destination_path}/{file_name}')
-        self._scp_command(file_dir + file_name, f'/sdcard/sync/{self.destination_path}/{file_name}')
-    
-    def send_new_files(self, source_path):
-        print('Scanning existing files on destination phone...')
-        self._scan_destination_files()
-        print('Found', len(self.existing_filenames), 'files.')
-        print('Scanning files from source directory...')
-        self._scan_source_files(source_path)
-        print('Found', self.source_files_pq.qsize(), 'new files.')
 
+    def _copy_over_file(self, file_dir, file_name, size=None):
+        if size:
+            available_space = self.storage_limit_bytes - self.size_of_existing_files
+            if available_space < size:
+                self._make_space(size - available_space)
+        print(
+            "Copying",
+            file_dir + file_name,
+            "to",
+            f"/sdcard/sync/{self.destination_path}/{file_name}",
+        )
+        self._scp_command(
+            file_dir + file_name, f"/sdcard/sync/{self.destination_path}/{file_name}"
+        )
+
+    def send_new_files(self, source_path):
+        # Scan all existing files on phone
+        print("Scanning existing files on destination phone...")
+        self._scan_destination_files()
+
+        # Scan source files
+        print("Scanning files from source directory...")
+        self._scan_source_files()
+        print("Found", len(self.source_files), "new files.")
+
+        if not self.source_files:
+            # There are no new files to copy over.
+            return
+
+        # Delete some old files
         available_space = self.storage_limit_bytes - self.size_of_existing_files
         if available_space < self.size_of_new_files:
+            print("Making some space.")
             self._make_space(self.size_of_new_files - available_space)
-        while not self.source_files_pq.empty():
-            _, filename, size_bytes = self.source_files_pq.get()
+
+        # Copy over new files
+        print("Copying over new files.")
+        for _, filename, size_bytes in self.source_files:
             self._copy_over_file(source_path, filename, size_bytes)
 
+        # Scan all existing files on phone
+        print("There are new files copied over. Recheck the integrity of the files.")
+        self._scan_destination_files()
 
-def create_client(ip, key_path, destination_path, limit, port=22, username='root'):
+        # Check their integrity with the source files
+        self._check_integrity(source_path)
+        while self.bad_files:
+            print("Found bad files:", self.bad_files)
+            print("Copying the bad files over again.")
+            # Replace the bad files
+            for filename in self.bad_files:
+                self._delete_existing_file(filename)
+                self.copy_over_files(source_path, filename)
+
+            # Check integrity again
+            self.bad_files = []
+            print("Check the integrity of the files again.")
+            self._check_integrity(source_path)
+
+        # Write ready file
+        self._create_ready_file()
+
+
+def create_client(
+    ip, key_path, source_path, destination_path, limit, port=22, username="root"
+):
     # Create an SSH client instance
     ssh = paramiko.SSHClient()
 
@@ -140,18 +225,59 @@ def create_client(ip, key_path, destination_path, limit, port=22, username='root
     # Setup SCP
     scp = SCPClient(ssh.get_transport())
 
-    return Client(ip, ssh, scp, destination_path, limit, port, username)
+    return Client(ip, ssh, scp, source_path, destination_path, limit, port, username)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Copy new media to the phone and delete older files.')
-    parser.add_argument('--source', '-s', required=True, type=str, help='Path to the directory that contains all of the media files you want to sync.')
-    parser.add_argument('--destination', '-d', required=True, type=str, help='Directory name on the phone within /sync.')
-    parser.add_argument('--phone_ip', '--ip', required=True, type=str, help='IP address of the phone.')
-    parser.add_argument('--phone_port_num', '--port', required=True, type=int, help='Port number that the phone SSH daemon is listening on.')
-    parser.add_argument('--rsa_key_path', '--rsa', type=str, default='~/.ssh/id_rsa', help='Path to your SSH key.')
-    parser.add_argument('--storage_limit_gb', '--limit', type=int, default=30, help='Limit in GiBs. Deletes the oldest files from the destination to stay under this limit. Defaults to 30GiB.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Copy new media to the phone and delete older files."
+    )
+    parser.add_argument(
+        "--source",
+        "-s",
+        required=True,
+        type=str,
+        help="Path to the directory that contains all of the media files you want to sync.",
+    )
+    parser.add_argument(
+        "--destination",
+        "-d",
+        required=True,
+        type=str,
+        help="Directory name on the phone within /sync.",
+    )
+    parser.add_argument(
+        "--phone_ip", "--ip", required=True, type=str, help="IP address of the phone."
+    )
+    parser.add_argument(
+        "--phone_port_num",
+        "--port",
+        required=True,
+        type=int,
+        help="Port number that the phone SSH daemon is listening on.",
+    )
+    parser.add_argument(
+        "--rsa_key_path",
+        "--rsa",
+        type=str,
+        default="~/.ssh/id_rsa",
+        help="Path to your SSH key.",
+    )
+    parser.add_argument(
+        "--storage_limit_gb",
+        "--limit",
+        type=int,
+        default=30,
+        help="Limit in GiBs. Deletes the oldest files from the destination to stay under this limit. Defaults to 30GiB.",
+    )
     args = parser.parse_args()
 
-    client = create_client(args.phone_ip, args.rsa_key_path, args.destination, args.storage_limit_gb, args.phone_port_num)
+    client = create_client(
+        args.phone_ip,
+        args.rsa_key_path,
+        args.source,
+        args.destination,
+        args.storage_limit_gb,
+        args.phone_port_num,
+    )
     client.send_new_files(args.source)
--- test_sync.py
+++ test_sync.py
@@ -2,18 +2,21 @@ import unittest
 from sync import Client, record_separator
 from unittest.mock import patch
 
+
 def read_some_files():
     output = []
     for i in range(10, 0, -1):
-        output.append(f'{i}{record_separator}{i}{record_separator}{i}')
-    return '\n'.join(output)
+        output.append(
+            f"{i}{record_separator}{i}{record_separator}{i}{record_separator}{i}"
+        )
+    return "\n".join(output)
+
 
 class ClientTestModule(unittest.TestCase):
-
     def setUp(self):
-        self.client = Client('0.0.0.0', None, None, 'foo', 1)
+        self.client = Client("0.0.0.0", None, None, "boo", "foo", 1)
 
-    @patch.object(Client, '_delete_existing_file')
+    @patch.object(Client, "_delete_existing_file")
     def test_make_space_empty(self, mock_delete_existing_file):
         self.assertEqual(self.client.size_of_existing_files, 0)
         self.client._make_space(0)
@@ -23,7 +26,7 @@ class ClientTestModule(unittest.TestCase):
 
         mock_delete_existing_file.assert_not_called()
 
-    @patch.object(Client, '_ssh_command')
+    @patch.object(Client, "_ssh_command")
     def test_make_space(self, mock_ssh_command):
         mock_ssh_command.return_value = read_some_files()
         self.client._scan_destination_files()
@@ -35,31 +38,31 @@ class ClientTestModule(unittest.TestCase):
         # Deletes the oldest file.
         self.client._make_space(1)
         self.assertEqual(len(self.client.existing_filenames), 9)
-        self.assertTrue('1' not in self.client.existing_filenames)
+        self.assertTrue("1" not in self.client.existing_filenames)
         self.assertEqual(self.client.size_of_existing_files, 54)
 
         # Previously deleted file is not re-deleted.
         self.client._make_space(1)
         self.assertEqual(len(self.client.existing_filenames), 8)
-        self.assertTrue('2' not in self.client.existing_filenames)
+        self.assertTrue("2" not in self.client.existing_filenames)
         self.assertEqual(self.client.size_of_existing_files, 52)
-        
+
         # Exactly 3 file's worth of space freed.
-        self.client._make_space(3+4+5)
+        self.client._make_space(3 + 4 + 5)
         self.assertEqual(len(self.client.existing_filenames), 5)
         self.assertEqual(self.client.size_of_existing_files, 40)
-        
+
         # Two file's worth + 1 byte so it would still need to delete a third.
-        self.client._make_space(6+7+1)
+        self.client._make_space(6 + 7 + 1)
         self.assertEqual(len(self.client.existing_filenames), 2)
         self.assertEqual(self.client.size_of_existing_files, 19)
-        
+
         # Exceed remaining files should just delete everything and not error
         self.client._make_space(9999999)
         self.assertEqual(len(self.client.existing_filenames), 0)
         self.assertEqual(self.client.size_of_existing_files, 0)
 
-    @patch.object(Client, '_ssh_command')
+    @patch.object(Client, "_ssh_command")
     def test_scan_destination_files(self, mock_ssh_command):
         mock_ssh_command.return_value = read_some_files()
 
@@ -68,9 +71,12 @@ class ClientTestModule(unittest.TestCase):
         self.assertEqual(len(self.client.existing_filenames), 10)
         self.assertEqual(self.client.size_of_existing_files, 55)
 
-        for i in range(1, 11):
-            self.assertEqual(self.client.existing_files_pq.get(), (i, str(i), i))
+        # for i in range(1, 11):
+        for i, item in enumerate(self.client.existing_files):
+            i += 1
+            self.assertEqual(item, (i, str(i), i, str(i)))
             self.assertTrue(str(i) in self.client.existing_filenames)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
